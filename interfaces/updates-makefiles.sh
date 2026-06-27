fc7ea1d7189896ddb482e058eee632c8f8fe19b6
Huawei_Dev <surdu_petru@yahoo.com>
2026-06-27 21:43:00 +0200

    interfaces: bring back android.hardware.vibrator@1.0 * needed for vendor.huawei.hardware.hwvibrator

--- interfaces/updates-makefiles.sh
+++ interfaces/updates-makefiles.sh
@@ -3,4 +3,5 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.huawei:device/huawei/compat/interfaces/huawei"
+  "vendor.huawei:device/huawei/compat/interfaces/huawei" \
+  "android.hardware:device/huawei/compat/interfaces/android/hardware"
